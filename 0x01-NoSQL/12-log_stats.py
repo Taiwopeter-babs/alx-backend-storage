194ec41497d587afd6e9afebc46dd4d94495d32a
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-17 20:23:32 +0100

    Add all queries

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Show statistics about nginx logs in mongodb"""
+from pymongo import MongoClient
+
+
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def show_nginx_stats(mongo_collection, method_option=None):
+    """Show formatted nginx stats in mongo db
+    recursively printing stats w.r.t the methods in METHODS
+    """
+
+    num_of_docs = mongo_collection.count_documents({})
+
+    # print number of logs
+    print("{} logs".format(num_of_docs))
+
+    print('Methods:')
+
+    for method in METHODS:
+        method_count = mongo_collection.count_documents(
+            {"method": {"$regex": method}}
+        )
+        print("\t{}: {}".format(method, method_count))
+
+    status_path_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_path_check} status check")
+
+
+if __name__ == '__main__':
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    show_nginx_stats(nginx_collection)

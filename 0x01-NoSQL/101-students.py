a17593a8cfa9a2e51ee2f842c68475b6578f3ed0
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-18 18:14:18 +0100

    return aggregated data

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Sorts all students by average score """
+
+
+def top_students(mongo_collection):
+    """ sorts students by average score """
+    return mongo_collection.aggregate([
+        {"$project": {
+            "average_score": {"$avg": "$topics.score"}
+        }
+        },
+        {"$sort":
+            {"average_score": -1}
+         }
+    ])

de06173bd00c3f40b3db068125d8e071a07d8912
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-07-19 14:12:07 +0100

    track access amount of url and set expiration of cached time

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""track page visits and cache"""
+from functools import wraps
+from redis import Redis
+import requests
+from typing import Callable
+
+
+url_arg = 'http://slowwly.robertomurray.co.uk'
+redis_cli = Redis()
+
+
+def track_access(func: Callable):
+    """Decorator function to track url access"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """Wrapper function"""
+        url_key = "count:{}".format(*args)
+        print(url_key)
+        incr_val = redis_cli.incr(url_key, 1)
+        redis_cli.set(url_key, incr_val, ex=10, xx=True)
+        return func(args, **kwargs)
+    return wrapper
+
+
+@track_access
+def get_page(url: str) -> str:
+    """retrieve url page"""
+    try:
+        req = requests.get(url)
+        return req.text
+    except Exception:
+        return ""
+
+
+res = get_page(url_arg)
+print(res)
